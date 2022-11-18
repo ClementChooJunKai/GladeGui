744ec5edb959df814871c2f998e659496a350d7c
ClementChooJunKai <clementchoo78@gmail.com>
2022-11-18 18:59:07 +0800

    Get basic function out using c and renamed name

--- GuiC.c
+++ GuiC.c
@@ -2,25 +2,25 @@
 #include <string.h>
 
 GtkWidget *window;
-GtkWidget *boxPvP;
+GtkWidget *mainMenu;
 GtkWidget *player_vs_player;
 GtkWidget *player_vs_computer_easy;
 GtkWidget *player_vs_computer_medium;
 GtkWidget *player_vs_computer_hard;
 GtkWidget *quit;
-GtkWidget *fixedMainMenu;
-GtkWidget *ttcpage;
-GtkWidget *ttcgrid;
-GtkWidget *ttc1;
+GtkWidget *fixed;
+GtkWidget *tttpage;
+GtkWidget *tttgrid;
+GtkWidget *ttt1;
 GtkWidget *child1;
-GtkWidget *ttc2;
-GtkWidget *ttc3;
-GtkWidget *ttc4;
-GtkWidget *ttc5;
-GtkWidget *ttc6;
-GtkWidget *ttc7;
-GtkWidget *ttc8;
-GtkWidget *ttc9;
+GtkWidget *ttt2;
+GtkWidget *ttt3;
+GtkWidget *ttt4;
+GtkWidget *ttt5;
+GtkWidget *ttt6;
+GtkWidget *ttt7;
+GtkWidget *ttt8;
+GtkWidget *ttt9;
 GtkWidget *image;
 
 
@@ -48,19 +48,19 @@ int main (int argc,char *argv[]){
 
     //gtk_widget_show (fixed);
     //creating a fixed container
-    fixedMainMenu = gtk_fixed_new ();
+    fixed = gtk_fixed_new ();
     
-    gtk_widget_set_name(fixedMainMenu,"fixedMainMenu");
+    gtk_widget_set_name(fixed,"fixed");
     
-    gtk_container_add (GTK_CONTAINER (window), fixedMainMenu);
+    gtk_container_add (GTK_CONTAINER (window), fixed);
     
     //creating box container
-    boxPvP = gtk_fixed_new();
-    ttcpage = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
-    gtk_container_add(GTK_CONTAINER(fixedMainMenu),boxPvP);
-    gtk_container_add (GTK_CONTAINER (fixedMainMenu), ttcpage);
-    gtk_widget_set_name(boxPvP,"boxPvP");
-    gtk_widget_set_name(ttcpage,"ttcpage");
+    mainMenu = gtk_fixed_new();
+    tttpage = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
+    gtk_container_add(GTK_CONTAINER(fixed),mainMenu);
+    gtk_container_add (GTK_CONTAINER (fixed), tttpage);
+    gtk_widget_set_name(mainMenu,"mainMenu");
+    gtk_widget_set_name(tttpage,"tttpage");
    
    
     
@@ -68,75 +68,75 @@ int main (int argc,char *argv[]){
     //creating elements in main menu 
     player_vs_player = gtk_button_new_with_label("Player Vs Player");
     player_vs_computer_easy = gtk_button_new_with_label("Player Versus Computer(Easy)");
-    player_vs_computer_medium= gtk_button_new_with_label("Player Versus Computer(Medium)");
-    player_vs_computer_hard= gtk_button_new_with_label("Player Versus Computer(Hard)");
+    player_vs_computer_medium = gtk_button_new_with_label("Player Versus Computer(Medium)");
+    player_vs_computer_hard = gtk_button_new_with_label("Player Versus Computer(Hard)");
     quit= gtk_button_new_with_label("Back");
-    gtk_container_add(GTK_CONTAINER(boxPvP),player_vs_computer_easy);
+    gtk_container_add(GTK_CONTAINER(mainMenu),player_vs_computer_easy);
     gtk_widget_set_name(player_vs_computer_easy,"player_vs_computer_easy");
-    gtk_container_add(GTK_CONTAINER(boxPvP),player_vs_player);
+    gtk_container_add(GTK_CONTAINER(mainMenu),player_vs_player);
     gtk_widget_set_name(player_vs_player,"player_vs_player");
-    gtk_container_add(GTK_CONTAINER(boxPvP),quit);
+    gtk_container_add(GTK_CONTAINER(mainMenu),quit);
     gtk_widget_set_name(quit,"quit");
-    gtk_container_add(GTK_CONTAINER(boxPvP),player_vs_computer_hard);
+    gtk_container_add(GTK_CONTAINER(mainMenu),player_vs_computer_hard);
     gtk_widget_set_name(player_vs_computer_hard,"player_vs_computer_hard");
-    gtk_container_add(GTK_CONTAINER(boxPvP),player_vs_computer_medium);
+    gtk_container_add(GTK_CONTAINER(mainMenu),player_vs_computer_medium);
     gtk_widget_set_name(player_vs_computer_medium,"player_vs_computer_medium");
 
     
 
     //Tic tac toe page 
-    //creating elements in page ttc
-    ttcgrid = gtk_grid_new();
-    gtk_widget_set_name(ttcgrid,"ttcgrid");
-    gtk_container_add(GTK_CONTAINER(ttcpage),ttcgrid);
+    //creating elements in page ttt
+    tttgrid = gtk_grid_new();
+    gtk_widget_set_name(tttgrid,"tttgrid");
+    gtk_container_add(GTK_CONTAINER(tttpage),tttgrid);
     
 
-    ttc1 = gtk_button_new_with_label("");
-    ttc2 = gtk_button_new_with_label("TTC2");
-    ttc3 = gtk_button_new_with_label("TTC3");
-    ttc4 = gtk_button_new_with_label("TTC4");
-    ttc5 = gtk_button_new_with_label("TTC5");
-    ttc6 = gtk_button_new_with_label("TTC6");
-    ttc7 = gtk_button_new_with_label("TTC7");
-    ttc8 = gtk_button_new_with_label("TTC8");
-    ttc9 = gtk_button_new_with_label("TTC9");
+    ttt1 = gtk_button_new_with_label("");
+    ttt2 = gtk_button_new_with_label("ttt2");
+    ttt3 = gtk_button_new_with_label("ttt3");
+    ttt4 = gtk_button_new_with_label("ttt4");
+    ttt5 = gtk_button_new_with_label("ttt5");
+    ttt6 = gtk_button_new_with_label("ttt6");
+    ttt7 = gtk_button_new_with_label("ttt7");
+    ttt8 = gtk_button_new_with_label("ttt8");
+    ttt9 = gtk_button_new_with_label("ttt9");
 
-    gtk_widget_set_name(ttc1,"ttc1");
-    gtk_widget_set_name(ttc2,"ttc2");
-    gtk_widget_set_name(ttc3,"ttc3");
-    gtk_widget_set_name(ttc4,"ttc4");
-    gtk_widget_set_name(ttc5,"ttc5");
-    gtk_widget_set_name(ttc6,"ttc6");
-    gtk_widget_set_name(ttc7,"ttc7");
-    gtk_widget_set_name(ttc8,"ttc8");
-    gtk_widget_set_name(ttc9,"ttc9");
+    gtk_widget_set_name(ttt1,"ttt1");
+    gtk_widget_set_name(ttt2,"ttt2");
+    gtk_widget_set_name(ttt3,"ttt3");
+    gtk_widget_set_name(ttt4,"ttt4");
+    gtk_widget_set_name(ttt5,"ttt5");
+    gtk_widget_set_name(ttt6,"ttt6");
+    gtk_widget_set_name(ttt7,"ttt7");
+    gtk_widget_set_name(ttt8,"ttt8");
+    gtk_widget_set_name(ttt9,"ttt9");
 
     //adding image to button
-    //gtk_button_set_always_show_image (GTK_BUTTON (ttc1), TRUE);
-    //gtk_button_set_image(GTK_BUTTON(ttc1),image);
+    //gtk_button_set_always_show_image (GTK_BUTTON (ttt1), TRUE);
+    //gtk_button_set_image(GTK_BUTTON(ttt1),image);
 
 
-    gtk_grid_set_row_homogeneous(GTK_GRID(ttcgrid),TRUE);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc1),1,0,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc2),2,0,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc3),3,0,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc4),1,1,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc5),2,1,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc6),3,1,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc7),1,2,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc8),2,2,1,1);
-    gtk_grid_attach(GTK_GRID(ttcgrid),GTK_BUTTON(ttc9),3,2,1,1);
+    gtk_grid_set_row_homogeneous(GTK_GRID(tttgrid),TRUE);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt1),1,0,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt2),2,0,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt3),3,0,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt4),1,1,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt5),2,1,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt6),3,1,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt7),1,2,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt8),2,2,1,1);
+    gtk_grid_attach(GTK_GRID(tttgrid),GTK_BUTTON(ttt9),3,2,1,1);
 
 
 
 
     //Moving elements
-    gtk_fixed_move(GTK_FIXED(fixedMainMenu),ttcpage,250,120);
-    gtk_fixed_move(GTK_FIXED(boxPvP),player_vs_player,300,50);
-    gtk_fixed_move(GTK_FIXED(boxPvP),player_vs_computer_easy,300,100);
-    gtk_fixed_move(GTK_FIXED(boxPvP),player_vs_computer_medium,300,150);
-    gtk_fixed_move(GTK_FIXED(boxPvP),player_vs_computer_hard,300,200);
-    gtk_fixed_move(GTK_FIXED(boxPvP),quit,300,250);
+    gtk_fixed_move(GTK_FIXED(fixed),tttpage,250,120);
+    gtk_fixed_move(GTK_FIXED(mainMenu),player_vs_player,300,50);
+    gtk_fixed_move(GTK_FIXED(mainMenu),player_vs_computer_easy,300,100);
+    gtk_fixed_move(GTK_FIXED(mainMenu),player_vs_computer_medium,300,150);
+    gtk_fixed_move(GTK_FIXED(mainMenu),player_vs_computer_hard,300,200);
+    gtk_fixed_move(GTK_FIXED(mainMenu),quit,300,250);
      
     
 
@@ -151,8 +151,8 @@ int main (int argc,char *argv[]){
   
     g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
     gtk_widget_show_all(window);
-   //gtk_widget_hide(boxPvP);
-    gtk_widget_hide(ttcpage);
+   //gtk_widget_hide(mainMenu);
+    gtk_widget_hide(tttpage);
 
     g_signal_connect(player_vs_player,"clicked", G_CALLBACK(hideMenu),NULL);
     //gtk code comes here
@@ -163,6 +163,6 @@ int main (int argc,char *argv[]){
 }
 
 void hideMenu(){
-        gtk_widget_hide(boxPvP);
-        gtk_widget_show(ttcpage);
+        gtk_widget_hide(mainMenu);
+        gtk_widget_show(tttpage);
 }
